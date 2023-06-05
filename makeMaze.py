47345d05e3b6a86d06e1e9d7b51b578bc163e411
demelcjohn <demal.john007@gmail.com>
2023-06-05 06:49:07 +0530

    finding route

--- makeMaze.py
+++ makeMaze.py
@@ -1,7 +1,14 @@
+import random
 import numpy as np
 from displayMaze import displayMaze
 
 stack = []
+neighbour = {
+    0: (-1, -1),
+    1: (-1, 1),
+    2: (1, -1),
+    3: (1, 1)
+}
 
 
 def makeMaze(n, m):
@@ -16,7 +23,25 @@ def makeMaze(n, m):
 def correctWay(maze):
     n, m = maze.shape
     curr = (0, 0)
+    maze[0][0] = 1
     stack.append(curr)
     while curr != (n-1, n-1):
-        print("hello")
-        curr = (n-1, n-1)
+        okneighbours = [0, 1, 2, 3]
+        while True:
+            nei = random.choice(okneighbours)
+            nIndex = neighbour[nei]
+            print(nIndex)
+            if maze[curr[0]+nIndex[0]][curr[1]+nIndex[1]] == 0:
+                if checkNeighbours(maze, curr[0]+nIndex[0], curr[1]+nIndex[1]):
+                    curr = (curr[0]+nIndex[0], curr[1]+nIndex[1])
+                    stack.append(curr)
+                    break
+            else:
+                okneighbours.pop(nei)
+            if okneighbours == []:
+                stack.pop()
+                curr = stack[-1]
+                break
+
+
+def checkNeighbours(maze, x, y):
