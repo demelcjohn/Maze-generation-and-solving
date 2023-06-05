e26e9d1de58e79951443edad70b9475540df7ae9
demelcjohn <demal.john007@gmail.com>
2023-06-05 06:03:54 +0530

    making correctWay

--- makeMaze.py
+++ makeMaze.py
@@ -1,10 +1,22 @@
 import numpy as np
 from displayMaze import displayMaze
 
+stack = []
+
 
 def makeMaze(n, m):
     maze = np.empty((n, m), dtype=int)
     maze = np.zeros((n, m))
     maze = maze.astype(int)
-
     displayMaze(maze)
+
+    maze = correctWay(maze)
+
+
+def correctWay(maze):
+    n, m = maze.shape
+    curr = (0, 0)
+    stack.append(curr)
+    while curr != (n-1, n-1):
+        print("hello")
+        curr = (n-1, n-1)
