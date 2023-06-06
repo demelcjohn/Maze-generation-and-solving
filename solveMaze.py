c27906a483160df1021956d24dad93022b338514
demelcjohn <demal.john007@gmail.com>
2023-06-06 07:59:53 +0530

    added getNeighbours

--- solveMaze.py
+++ solveMaze.py
@@ -1,2 +1,35 @@
+class correctWay:
+    def __init__(self, cell, neighbours):
+        self.cell = cell
+        self.neighbours = neighbours
+
+
+stack = []
+
+
 def solveMaze(maze):
-    print("qwerty")
+    n, m = maze.shape
+    curr = (0, 0)
+    neighbours = getNeighbours(maze, curr)
+    way = correctWay(curr, neighbours)
+    stack.append(way)
+    while curr[0] != n-1 and curr[1] != m-1:
+        print("Hello")
+
+
+def getNeighbours(maze, x, y):
+    n, m = maze.shape
+    nei = []
+    if x >= 0:
+        if maze[x-1][y] == 1:
+            nei.append((x, y))
+    if y >= 0:
+        if maze[x][y-1] == 1:
+            nei.append((x, y))
+    if x < n:
+        if maze[x+1][y] == 1:
+            nei.append((x, y))
+    if y < m:
+        if maze[x][y+1] == 1:
+            nei.append((x, y))
+    return nei
