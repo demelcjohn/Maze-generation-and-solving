9d8fe15d002633389b2c0e845c75f154ccc86910
demelcjohn <demal.john007@gmail.com>
2023-06-06 08:52:21 +0530

    solveMaze initial

--- solveMaze.py
+++ solveMaze.py
@@ -1,7 +1,4 @@
-class correctWay:
-    def __init__(self, cell, neighbours):
-        self.cell = cell
-        self.neighbours = neighbours
+from displayMaze import displayMaze
 
 
 stack = []
@@ -10,26 +7,33 @@ stack = []
 def solveMaze(maze):
     n, m = maze.shape
     curr = (0, 0)
-    neighbours = getNeighbours(maze, curr)
-    way = correctWay(curr, neighbours)
-    stack.append(way)
+    maze[curr[0]][curr[1]] = 2
+    stack.append(curr)
     while curr[0] != n-1 and curr[1] != m-1:
-        print("Hello")
+        nei = getNeighbour(maze, curr)
+        if nei == (-1, -1):
+            maze[curr[0]][curr[1]] = 1
+            stack.pop()
+        else:
+            curr = nei
+            maze[curr[0]][curr[1]] = 2
+            stack.append(curr)
+        displayMaze(maze)
 
 
-def getNeighbours(maze, x, y):
+def getNeighbour(maze, pos):
     n, m = maze.shape
-    nei = []
+    x, y = pos
     if x >= 0:
         if maze[x-1][y] == 1:
-            nei.append((x, y))
+            return (x-1, y)
     if y >= 0:
         if maze[x][y-1] == 1:
-            nei.append((x, y))
+            return (x, y-1)
     if x < n:
         if maze[x+1][y] == 1:
-            nei.append((x, y))
+            return (x+1, y)
     if y < m:
         if maze[x][y+1] == 1:
-            nei.append((x, y))
-    return nei
+            return (x, y+1)
+    return (-1, -1)
