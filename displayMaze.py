10135527aa6ef240249e2ab1c62d2085dcf1b7a1
demelcjohn <demal.john007@gmail.com>
2023-06-06 18:32:02 +0530

    display changed

--- displayMaze.py
+++ displayMaze.py
@@ -1,24 +1,24 @@
 def displayMaze(maze):
     n, m = maze.shape
     for i in range(m+2):
-        print("#", end=" ")
+        print("+", end=" ")
     print()
     for i in range(n):
-        print("#", end=" ")
+        print("+", end=" ")
         for j in range(m):
             if i == 0 and j == 0:
                 print("S", end=" ")
             elif i == n-1 and j == m-1:
                 print("E", end=" ")
             elif maze[i][j] == 0:
-                print("#", end=" ")
+                print("+", end=" ")
             elif maze[i][j] == 1:
                 print(" ", end=" ")
             elif maze[i][j] == 2:
-                print("*", end=" ")
+                print("@", end=" ")
             elif maze[i][j] == 3:
                 print(" ", end=" ")
-        print("#")
+        print("+")
     for i in range(m+2):
-        print("#", end=" ")
+        print("+", end=" ")
     print()
